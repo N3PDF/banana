b37a2810179ac749a7801f048504de50f9051e86
Alessandro Candido <candido.ale@gmail.com>
2020-10-30 11:58:01 +0100

    Init base docs infrastructure

---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. banana documentation master file, created by
+   sphinx-quickstart on Fri Oct 30 11:51:34 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to banana's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- setup.py
+++ setup.py
@@ -4,18 +4,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="banana-hep",
-    author="F. Hekhorn, A.Candido",
+    author="Felix Hekhorn, Alessandro Candido",
     version="0.1.0",
     description="mm yummy banana",
-    package_data={"banana": ["data/templatePDF.dat", "data/templatePDF.info", "data/theory_template.yaml"]},
+    package_data={
+        "banana": [
+            "data/templatePDF.dat",
+            "data/templatePDF.info",
+            "data/theory_template.yaml",
+        ]
+    },
     package_dir={"": "src"},
     packages=find_packages("src"),
-    install_requires=[
-        "numpy",
-        "rich",
-        "pyyaml",
-        "tinydb~=4.1"
-    ],
+    install_requires=["numpy", "rich", "pyyaml", "tinydb~=4.1"],
     entry_points={
         "console_scripts": [
             "generate_pdf=banana.data.generate_pdf:generate_pdf",
