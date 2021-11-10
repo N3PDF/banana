fe20a996c6d9e58d3757ded6965858e0868ea2fb
Andrea Barontini <andreab1997@yahoo.it>
2021-11-10 14:45:15 +0100

    Enable loop on members, init CLI test

--- src/banana/data/generate_pdf2/__init__.py
+++ src/banana/data/generate_pdf2/__init__.py
@@ -16,14 +16,16 @@ def cli():
 @click.argument("name")
 @click.argument("labels", nargs=-1, type=int)
 @click.option("-p", "--parent-pdf-set", default=None, help="parent pdf set")
+@click.option("-a", "--all", is_flag=True, help="generate all the members")
 @click.option("-i", "--install", is_flag=True, help="install into LHAPDF")
-def generate_pdf(name, labels, parent_pdf_set, install):
+def generate_pdf(name, labels, parent_pdf_set, all, install):
     """Entry point to :func:`make_filter_pdf` and :func:`make_debug_pdf`"""
-    # TODO allow to iterate ALL members
 
     pathlib.Path(name).mkdir(exist_ok=True)
     # labels = verify_labels(args.labels)
     # collect blocks
+    all_blocks = []
+    info = []
     if parent_pdf_set is None:
         # TODO use debug settings, i.e. x*(1-x) for everybody
         pass
@@ -32,11 +34,19 @@ def generate_pdf(name, labels, parent_pdf_set, install):
         pass
     else:
         info = load.load_info_from_file(parent_pdf_set)
-        blocks = load.load_blocks_from_file(parent_pdf_set, 0)
+        # iterate on members
+        for m in range(int(info["NumMembers"])):
+            # blocks = load.load_blocks_from_file(parent_pdf_set, 0)
+            all_blocks.append(load.load_blocks_from_file(parent_pdf_set, m))
+            if not all:
+                break
     # filter the PDF
-    new_blocks = filter.filter_pids(blocks, labels)
+    new_all_blocks = []
+    for b in all_blocks:
+        # new_blocks = filter.filter_pids(b, labels)
+        new_all_blocks.append(filter.filter_pids(b, labels))
     # write
-    export.dump_set(name, info, [new_blocks])
+    export.dump_set(name, info, new_all_blocks)
 
     # install
     if install:
---
+++ tests/test_data_generate_pdf_CLI.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pytest
+from click.testing import CliRunner
+from utils import lhapdf_path, test_pdf
+
+from banana.data.generate_pdf2 import cli
+
+# try:
+#     import lhapdf
+# except ImportError:
+#     pytest.skip("No LHAPDF interface around", allow_module_level=True)
+# TODO mark file skipped in coverage.py
+lhapdf = pytest.importorskip("lhapdf")
+runner = CliRunner()
+
+
+def test_genpdf_messages():
+    result = runner.invoke(cli, ["generate"])
+    assert "Error: Missing argument 'NAME'." in result.output
+    result = runner.invoke(cli, ["install"])
+    assert "Error: Missing argument 'NAME'." in result.output
+    result = runner.invoke(cli, ["generate", "--help"])
+    assert "-p, --parent-pdf-set TEXT" in result.output
+    assert "-a, --all" in result.output
+    assert "-i, --install" in result.output
