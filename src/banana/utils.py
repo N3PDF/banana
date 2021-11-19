14ae862fc6299f5f0c2176a00cd9b789a153c338
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2021-11-19 11:18:24 +0100

    Improve utils

--- src/banana/utils.py
+++ src/banana/utils.py
@@ -1,16 +1,11 @@
-import os
-import pathlib
 from contextlib import contextmanager
 
-import lhapdf
 
-# import pytest
-
-# TODO mark file skipped in coverage.py
-# lhapdf = pytest.importorskip("lhapdf")
 # lets follow the same spirit
 @contextmanager
 def lhapdf_path(newdir):
+    import lhapdf  # pylint: disable=import-error, import-outside-toplevel
+
     paths = lhapdf.paths()
     lhapdf.pathsPrepend(str(newdir))
     try:
