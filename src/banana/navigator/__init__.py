9e51bdd95df398ee828e0d2c91b083b5733981ca
Alessandro Candido <candido.ale@gmail.com>
2022-02-24 12:54:17 +0100

    Upgrade navigator launcher

--- src/banana/navigator/__init__.py
+++ src/banana/navigator/__init__.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
 import inspect
 
 import IPython
-from traitlets.config.loader import Config
+from traitlets.config import loader
 
-from .navigator import NavigatorApp, c, l, o, t, table_objects
+from .. import cfg
+from .navigator import c, l, o, t
 from .utils import compare_dicts
 
 help_vars = f"""t = "{t}" -> query theories
@@ -46,20 +48,54 @@ def register_globals(mod, app):
     mod.update(new_objs)
 
 
-def launch_navigator(pkg, bench=None):
-    c = Config()
+def launch_navigator(imports=None, cfg_path=None, skip_cfg=False, pylab=True):
+    """Launch navigator.
+
+    Parameters
+    ----------
+    imports: list(str)
+        name of packages/modules to be imported (the whole content is directly
+        imported in the global namespace); if ``None`` is considered to be
+        empty (default: ``None``)
+    cfg_path: str or os.PathLike
+        path to configuration file or containing folder (if using the
+        conventional name :data:`banana.cfg.name`); if ``None`` or pointing to a
+        non-existing file, automatic detection is attempted (default: ``None``)
+    skip_cfg: bool
+        whether to skip configurations loading; useful if this function is
+        invoked by a script, that is pre-loading configurations on its own
+        (default: ``False``)
+    pylab: bool
+        whether to load :mod:`matplotlib.pylab` members in the global namespace
+        (default: ``True``)
+
+    """
+    if not skip_cfg:
+        # configurations are loaded, to support inbokation from the CLI
+        cfg.cfg = cfg.load(cfg.detect(cfg_path))
+
+    # update ipython configurations to add custom banner
+    c = loader.Config()
     banner = f"""
         Welcome to {pkg} benchmark skript!
         call yelp() or h() for a brief overview.
-    """
+        """
     c.TerminalInteractiveShell.banner2 = inspect.cleandoc(banner) + "\n" * 2
 
-    if bench is not None:
-        init_cmds = [f"""from {bench}.navigator import *""", f"""from {pkg} import *"""]
-    else:
-        init_cmds = ["""from navigator import *""", f"""from {pkg} import *"""]
-    args = ["--pylab"]
+    # set initial imports
+    init_cmds = []
+    if imports is None:
+        imports = []
+
+    for imp in imports:
+        init_cmds.append(f"from {imp} import *")
+
+    # add commands to be execute while entering
+    args = []
+    if pylab:
+        args.append("--pylab")
     for cmd in init_cmds:
         args.append(f"--InteractiveShellApp.exec_lines={cmd}")
 
+    # launch and enter interpreter
     IPython.start_ipython(argv=args, config=c)
