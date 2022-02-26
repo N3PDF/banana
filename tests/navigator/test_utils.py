60b7b23f4d128345171e9d0eabefa731fc0e81e3
Alessandro Candido <candido.ale@gmail.com>
2022-02-26 12:43:12 +0100

    Test key exclusion in dict comparison

--- tests/navigator/test_utils.py
+++ tests/navigator/test_utils.py
@@ -2,7 +2,18 @@
 from banana.navigator import utils
 
 
-def test_nav_utils_underscore(capsys):
+def test_comparison(capsys):
+    a = dict(c=1, d=2, _e=3)
+    b = dict(c=1, d=3, _e=4)
+    utils.compare_dicts(a, b, exclude_underscored=True)
+    captured = capsys.readouterr()
+    assert captured.err == ""
+    assert "c" not in captured.out
+    assert "d" in captured.out
+    assert "_e" not in captured.out
+
+
+def test_comparison_underscore(capsys):
     a = dict(c=1, d=2, _e=3, f=4)
     b = dict(c=1, d=3, _e=4, g=5)
     utils.compare_dicts(a, b)
@@ -15,12 +26,12 @@ def test_nav_utils_underscore(capsys):
     assert "g" in captured.out
 
 
-def test_nav_utils(capsys):
-    a = dict(c=1, d=2, _e=3)
-    b = dict(c=1, d=3, _e=4)
-    utils.compare_dicts(a, b, exclude_underscored=True)
+def test_comparison_exclusion(capsys):
+    a = dict(c=1, d=2, e=3)
+    b = dict(c=1, d=3, e=4)
+    utils.compare_dicts(a, b, exclude_keys=["e"])
     captured = capsys.readouterr()
     assert captured.err == ""
     assert "c" not in captured.out
     assert "d" in captured.out
-    assert "_e" not in captured.out
+    assert "e" not in captured.out
