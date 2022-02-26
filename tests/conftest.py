74d51fd6fdcdc4a587bf46ea9238790f4a373e56
Alessandro Candido <candido.ale@gmail.com>
2022-02-26 13:05:52 +0100

    Mock lhapdf when not available

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import pathlib
 import shutil
+import sys
 
 import pytest
 import yaml
@@ -25,3 +26,29 @@ def banana_yaml(db):
     conf.write_text(yaml.dump(content), encoding="utf-8")
     yield conf
     conf.unlink(missing_ok=True)
+
+
+@pytest.fixture
+def lhapdf():
+    try:
+        import lhapdf
+
+        yield lhapdf
+    except ModuleNotFoundError:
+
+        class LHAPDF:
+            _paths = []
+
+            def paths(self):
+                return self._paths
+
+            def pathsPrepend(self, path):
+                self._paths.insert(0, path)
+
+            def setPaths(self, paths):
+                self._paths = paths
+
+        lhapdf = LHAPDF()
+        sys.modules["lhapdf"] = lhapdf
+        yield lhapdf
+        del sys.modules["lhapdf"]
