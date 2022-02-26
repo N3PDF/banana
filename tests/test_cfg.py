304332b5d8baf816eb83b916aa4c0465dbb13747
Alessandro Candido <candido.ale@gmail.com>
2022-02-26 12:28:39 +0100

    Clean up fake config file name after testing

--- tests/test_cfg.py
+++ tests/test_cfg.py
@@ -9,6 +9,14 @@ import pytest
 import banana.cfg
 
 
+@pytest.fixture
+def fake_name():
+    actual_name = banana.cfg.name
+    banana.cfg.name = "".join(random.choices(string.ascii_letters, k=200))
+    yield
+    banana.cfg.name = actual_name
+
+
 def test_detect(banana_yaml):
     # check the one in the current directory
     assert banana_yaml == banana.cfg.detect()
@@ -26,9 +34,10 @@ def test_detect(banana_yaml):
     assert path == banana.cfg.detect()
     path.unlink()
 
+
+def test_fail_detection(banana_yaml, fake_name):
     # if no file detected, raises
     # (and if the filename is absurd, detection will be impossible)
-    banana.cfg.name = "".join(random.choices(string.ascii_letters, k=100))
     with pytest.raises(FileNotFoundError, match="No configurations"):
         banana.cfg.detect()
 
