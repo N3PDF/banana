ce6697eba1be6857cabffdbbe76e188fb32cc884
Alessandro Candido <candido.ale@gmail.com>
2022-02-26 13:07:07 +0100

    Test lhapdf path context

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,11 @@
-import pytest
-
+# -*- coding: utf-8 -*-
 from banana import utils
 
-lhapdf = pytest.importorskip("lhapdf")
 
-
-def test_lhapdf_path(tmp_path):
+def test_lhapdf_path(tmp_path, lhapdf):
     assert str(tmp_path) not in lhapdf.paths()
+
     with utils.lhapdf_path(tmp_path):
         assert str(tmp_path) in lhapdf.paths()
+
     assert str(tmp_path) not in lhapdf.paths()
