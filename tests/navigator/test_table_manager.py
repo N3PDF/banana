a99b4e6fa44528b01fcb5307f6c9fe8aee1a34ef
Alessandro Candido <candido.ale@gmail.com>
2022-02-28 00:55:39 +0100

    Add remotion test

--- tests/navigator/test_table_manager.py
+++ tests/navigator/test_table_manager.py
@@ -1,13 +1,29 @@
 # -*- coding: utf-8 -*-
-import pytest
-
 from banana.navigator import table_manager as tm
 
 
 class TestTableManager:
     def test_init(self):
         session = "mysession"
-        table_object = "mytable"
-        tabman = tm.TableManager(session, table_object)
+        table = "mytable"
+        tabman = tm.TableManager(session, table)
         assert tabman.session == session
-        assert tabman.table_object == table_object
+        assert tabman.table_object == table
+
+    def test_remove(self, dbsession, tab_ciao):
+        tabman = tm.TableManager(dbsession, tab_ciao)
+
+        def assert_len(length):
+            with dbsession.begin():
+                available = dbsession.query(tab_ciao).all()
+                assert len(available) == length
+
+        assert_len(0)
+
+        with dbsession.begin():
+            newrec = tab_ciao(uid=42, name="leorio")
+            dbsession.add(newrec)
+        assert_len(1)
+
+        tabman.remove([42])
+        assert_len(0)
