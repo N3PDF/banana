f2e120d1e5768343f497024d96ac58416b871078
Alessandro Candido <candido.ale@gmail.com>
2022-02-27 12:28:53 +0100

    Test table manager creation

--- tests/navigator/test_table_manager.py
+++ tests/navigator/test_table_manager.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from banana.navigator import table_manager as t_m
+from banana.navigator import table_manager as tm
+
+
+class TestTableManager:
+    def test_init(self):
+        session = "mysession"
+        table_object = "mytable"
+        tabman = tm.TableManager(session, table_object)
+        assert tabman.session == session
+        assert tabman.table_object == table_object
